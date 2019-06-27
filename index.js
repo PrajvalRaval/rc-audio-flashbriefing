00d44d405feab3cc75210b86e1adc923b0fe67e7
Prajval Raval <41849970+PrajvalRaval@users.noreply.github.com>
2019-06-27 15:31:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,72 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const serverurl = process.env.SERVER_URL;
 const channelName = process.env.CHANNEL_NAME;
+const userName = process.env.USER_NAME;
+const passWord = process.env.PASSWORD;
 
 //Data Frequency Timing in ms & Title Text
 
-const cacheTimeout = 300000; //300000ms = 5mins
+const cacheTimeout = 120000; //300000ms = 5mins
 const flashBriefingTitle = 'Rocket Chat Flash Briefing';
+
+//Login
+
+const login = async (userName, passWord) =>
+  await axios
+  .post(`${ serverurl }/api/v1/login`, {
+    user: userName,
+    password: passWord,
+  })
+  .then((res) => res.data)
+  .then((res) => {
+    console.log(res);
+    const headers = {
+      'X-Auth-Token': res.data.authToken,
+      'X-User-Id': res.data.userId,
+    };
+    return headers;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const getFileType = async () =>
+  await axios
+  .get(`${ serverurl }/api/v1/channels.anonymousread?roomName=${ channelName }`)
+  .then((res) => res.data)
+  .then((res) => `${ res.messages[0].file.type }`)
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+//Get File Download URL
+
+const getLastMessageFileURL = async (channelName, headers) =>
+  await axios
+  .get(`${ serverurl }/api/v1/channels.messages?roomName=${ channelName }`, {
+    headers
+  })
+  .then((res) => res.data)
+  .then((res) => `https://bots.rocket.chat/file-upload/${ res.messages[0].file._id }/${res.messages[0].file.name}`)
+  .catch((err) => {
+      console.log(err.message);
+  });
+
+//Get S3 URL
+
+const getLastMessageFileDowloadURL = async (downloadURL, headers) =>
+  await axios
+  .get(downloadURL, {
+    headers
+  })
+  .then((response) => `${ response.request.res.responseUrl }`)
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 //PING ROUTE
 
-app.get('/ping', (req,res) => {
+app.get('/ping', (req, res) => {
 
   console.log('PING Request');
 
@@ -31,7 +88,7 @@ app.get('/ping', (req,res) => {
 
 //MAIN ROUTE
 
-app.get('/', (req,res) => {
+app.get('/', async (req, res) => {
 
   if (cache.get('message')) {
 
@@ -44,32 +101,69 @@ app.get('/', (req,res) => {
 
     console.log('Getting New Data From Rocket Chat Server');
 
-    return axios.get(`${ serverurl }/api/v1/channels.anonymousread?roomName=${ channelName }`)
-      .then(response => {
+    var filetype = await getFileType();
 
-        const responseJSON = JSON.stringify({
-          uid: response.data.messages[0]._id,
-          updateDate: response.data.messages[0].ts,
-          titleText: flashBriefingTitle,
-          mainText: response.data.messages[0].msg,
-          streamUrl: "https://s3.amazonaws.com/uploads.use1.cloud.rocket.chat/KqCQiHfeFaKdSEGvy/uploads/XLRg3gpC7uZBaHp5u/FpaaN9jwwJT9tsw2a/ep4npMnAkFuMgABbe?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAILPK6SHTK5RJZLHQ%2F20190627%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20190627T090125Z&X-Amz-Expires=120&X-Amz-Signature=28683b1b15e66bd1d6ad33f5180b02ef6c4e9223d6e3e330b0e3ce4862a721b4&X-Amz-SignedHeaders=host",
-          redirectionUrl: `${ serverurl }/channel/${ channelName }`
+    if (filetype) {
+      console.log(filetype);
+
+      var headers = await login(userName, passWord);
+      var downloadURL = await getLastMessageFileURL(channelName, headers);
+      var S3Url = await getLastMessageFileDowloadURL(downloadURL, headers);
+
+      return axios.get(`${ serverurl }/api/v1/channels.anonymousread?roomName=${ channelName }`)
+        .then(response => {
+
+          const responseJSON = JSON.stringify({
+            uid: response.data.messages[0]._id,
+            updateDate: response.data.messages[0].ts,
+            titleText: flashBriefingTitle,
+            mainText: response.data.messages[0].msg,
+            streamUrl: S3Url,
+            redirectionUrl: `${ serverurl }/channel/${ channelName }`
+          });
+
+          console.log('Storing Data In Memory.')
+          cache.put('message', responseJSON, cacheTimeout);
+
+          const result = JSON.parse(responseJSON);
+          return res.status(200).json(result);
+        })
+        .catch(err => {
+          console.log(err);
+          return res.status(200).json(err);
         });
 
-        console.log('Storing Data In Memory.')
-        cache.put('message', responseJSON, cacheTimeout);
+    } else {
+      console.log('text message');
 
-        const result = JSON.parse(responseJSON);
-        return res.status(200).json(result);
-      })
-      .catch(err => {
-        console.log(err);
-        return res.status(200).json(err);
-      });
+      return axios.get(`${ serverurl }/api/v1/channels.anonymousread?roomName=${ channelName }`)
+        .then(response => {
+
+          const responseJSON = JSON.stringify({
+            uid: response.data.messages[0]._id,
+            updateDate: response.data.messages[0].ts,
+            titleText: flashBriefingTitle,
+            mainText: response.data.messages[0].msg,
+            redirectionUrl: `${ serverurl }/channel/${ channelName }`
+          });
+
+          console.log('Storing Data In Memory.')
+          cache.put('message', responseJSON, cacheTimeout);
+
+          const result = JSON.parse(responseJSON);
+          return res.status(200).json(result);
+        })
+        .catch(err => {
+          console.log(err);
+          return res.status(200).json(err);
+        });
+    }
+
+
 
   }
 });
 
-app.listen(PORT,'0.0.0.0', function () {
+app.listen(PORT, '0.0.0.0', function () {
   console.log(`Server Now Listening on Port ${PORT}`);
 });
