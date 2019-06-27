0142015e5c4383864250693e9b9fdf23b4e7fc70
Prajval Raval <41849970+PrajvalRaval@users.noreply.github.com>
2019-06-27 16:58:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ app.get('/', async (req, res) => {
             updateDate: response.data.messages[0].ts,
             titleText: flashBriefingTitle,
             mainText: response.data.messages[0].msg,
-            streamUrl: __dirname + '/dist/foo.mp3',
+            streamUrl: 'https://rc-fb-usa.herokuapp.com/download'',
             redirectionUrl: `${ serverurl }/channel/${ channelName }`
           });
 
